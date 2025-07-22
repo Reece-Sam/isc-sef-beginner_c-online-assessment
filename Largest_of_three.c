6adb2918868f0562eef3843ce66d0dd23f4fe4f1
Reece-Sam <bonisamkelekoh@gmail.com>
2025-07-22 18:51:19 +0100

    This is the solution for question 3

---
+++ Largest_of_three.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+//The input 
+int main()
+//The process
+{ int a;
+  int b;
+  int c;
+  int largest = b;
+  
+  printf("Enter three numbers\n");
+  scanf("%d", &a);
+  scanf("%d", &b);
+  scanf("%d", &c);
+
+  if (a > b && a > b){    
+    printf("d% is the largest.\n", a);
+  } 
+
+  else if (b > a && b > c){
+    printf("d% is the largest.\n", b);
+  } 
+
+  else { 
+    printf("d% is the largest.\n", c);
+  } 
+              
+//The output
+ 
+  return 0;
+} 
